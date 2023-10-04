9603450489d3b340f4976a6af7d488626f7b890c
Ars Vladimirov <grokhael@gmail.com>
2023-10-04 06:04:17 +0000

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "ADE20K"
 PROJECT_NAME_FULL: str = "ADE20K"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
